4b16f63385ba239512c8dbd72d13a544cecd93a8
happyporcupines <skibble11@gmail.com>
2026-03-19 21:14:45 -0500

    adding rectangles

--- js/main.js
+++ js/main.js
@@ -10,6 +10,23 @@ window.onload = function(){
         .attr("width", w) //assign the width
         .attr("height", h) //assign the height
         .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-        .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+        .style("background-color", "rgba(0,0,0,0.2)") //only put a semicolon at the end of the block!
+        .append("rect") //add a <rect> element
+        .attr("width", 800) //rectangle width
+        .attr("height", 400) //rectangle height
+    //Example 1.8 line 1...innerRect block
+    var innerRect = container.append("rect")
+        .datum(400) //a single value is a DATUM
+        .attr("width", function(d){ //rectangle width
+            return d * 2; //400 * 2 = 800
+        })
+        .attr("height", function(d){ //rectangle height
+            return d; //400
+        })
+        .attr("class", "innerRect") //class name
+        .attr("x", 50) //position from left on the x (horizontal) axis
+        .attr("y", 50) //position from top on the y (vertical) axis
+        .style("fill", "#FFFFFF"); //fill color
 
 };
+
