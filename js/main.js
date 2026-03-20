75aa18e412eaff27c09be87b44b29f338ac3bc0b
happyporcupines <skibble11@gmail.com>
2026-03-19 21:00:20 -0500

    adding single script to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-// Add all scripts to the JS folder
+//execute script when window is loaded
+window.onload = function(){
+
+    var container = d3.select("body") //get the <body> element from the DOM
+        .append("svg"); //put a new svg in the body
+
+};
